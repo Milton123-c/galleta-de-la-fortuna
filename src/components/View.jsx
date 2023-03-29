14dc6940cd76118206e0afefc98b23e583297181
Milton123-c <miltonmercado92@gmail.com>
2023-03-28 18:25:22 -0600

    version 0.5

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,26 +1,43 @@
-import React from 'react'
+import React from "react";
 
-export const View = ({galleta}) => {
-
-  const {author, phrase} = galleta;
+export const View = ({ galleta }) => {
+  const { author, phrase } = galleta;
 
   return (
-    <div className='view'>
-      <svg className='svg__top' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 340"><path fill="#FFF" fillOpacity="1" d="M0,320L80,266.7C160,213,320,107,480,101.3C640,96,800,192,960,213.3C1120,235,1280,181,1360,154.7L1440,128L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
-     
-    
-    <svg className='svg__buttom' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fillOpacity="1" d="M0,320L80,266.7C160,213,320,107,480,101.3C640,96,800,192,960,213.3C1120,235,1280,181,1360,154.7L1440,128L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
-
-    <div className='svg__center'></div>
-
-    <div className='text__container'>
+    <div className="view">
+      <svg
+        className="bandera"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        x="0px"
+        y="0px"
+        width="500.0px"
+        height="250.0px"
+        viewBox="0 0 500.0 250.0"
+        enableBackground="new 0 0 500.0 250.0"
+        xmlSpace="preserve"
+      >
+        <path
+          fill="#FFFFFF"
+          stroke="#000000"
+          fillOpacity="1.000"
+          strokeOpacity="1.000"
+          fillRule="nonzero"
+          strokeWidth="0.0"
+          strokeLinejoin="miter"
+          strokeLinecap="square"
+          d="M485.59,198.18L485.59,18.41C411.52,56.10,336.98,56.10,270.64,33.33C190.83,0.17,92.18,-6.21,20.16,39.48L20.16,219.25C92.18,173.56,190.83,179.94,270.64,213.10C336.98,235.86,411.52,235.86,485.59,198.18z"
+        />
+      </svg>
+      <div className="text__container">
         <p>{phrase}</p>
-    </div>
+      </div>
 
-    <div className='comillas'>
-    <i className='bx bxs-quote-single-right'></i>
-    <i className='bx bxs-quote-single-right'></i>
+      <div className="comillas">
+        <i className="bx bxs-quote-single-right"></i>
+        <i className="bx bxs-quote-single-right"></i>
+      </div>
     </div>
-     </div>
-  )
-}
+  );
+};
